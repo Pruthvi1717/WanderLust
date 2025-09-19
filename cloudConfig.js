c384fd96ed39fd569ab074ed83af5efe6b869ca3
Pruthvi <9767119717p@gmail.com>
2025-09-19 15:00:53 +0530

    added Project

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,22 +1,21 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require("multer-storage-cloudinary-v2");
-
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
-      cloud_name: process.env.CLOUD_NAME,
-      api_key: process.env.CLOUD_API_KEY,
-      api_secret: process.env.CLOUD_API_SECRET
-})
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'WanderLust_DEV',
-    allowedFormats:["png","jpg","jpeg"]
-   },
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
-module.exports={
-    cloudinary,
-    storage
-}
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "WanderLust_DEV",
+    allowed_formats: ["png", "jpg", "jpeg"], // <-- lowercase "allowed_formats"
+  },
+});
+
+module.exports = {
+  cloudinary,
+  storage,
+};
